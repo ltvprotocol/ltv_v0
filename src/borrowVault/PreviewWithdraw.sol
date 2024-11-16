a3bd644a2e2188637e433f86cdd69fbf3a6b56b9
Andrey Sobol <andreysobol@protonmail.com>
2024-11-16 20:40:30 +0700

    pwb

---
+++ src/borrowVault/PreviewWithdraw.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+import "../State.sol";
+import "../Constants.sol";
+import "../Structs.sol";
+import "./totalAssets.sol";
+import "../ERC20.sol";
+import "../Cases.sol";
+import "../math/DepositWithdrawallBorrow.sol";
+
+abstract contract PreviewWithdraw is State, TotalAssets, ERC20, DepositWithdrawallBorrow {
+
+    using MulDiv for uint256;
+
+    function previewWithdraw(uint256 assets) external view returns (uint256 shares) {
+
+        int256 signedShares = previewDepositWithdrawallBorrow(int256(assets));
+        
+        if (signedShares < 0) {
+            return 0;
+        }
+
+        uint256 supply = totalSupply;
+
+        uint256 shares = supply == 0 ? assets : assets.mulDivDown(supply, totalAssets());
+
+        return shares;
+    }
+
+}
