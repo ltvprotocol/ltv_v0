14cfc16006d14c937a42ab5e2fac4c5ddfc9046d
Andrey Sobol <andreysobol@protonmail.com>
2024-11-16 20:37:48 +0700

    TotalA

---
+++ src/borrowVault/TotalAssets.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+import "../State.sol";
+import "../Constants.sol";
+import "../Structs.sol";
+
+abstract contract TotalAssets is State {
+
+    function totalAssets() public view returns (uint256) {
+        ConvertedAssets memory convertedAssets = recoverConvertedAssets();
+        int256 totalAssets = convertedAssets.collateral + convertedAssets.borrow;
+        return uint256(totalAssets);
+    }
+
+}
