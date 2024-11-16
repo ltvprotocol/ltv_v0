a19f1302902225d94387a47b7d9b281780796026
Andrey Sobol <andreysobol@protonmail.com>
2024-11-17 00:34:33 +0700

    Add 2 more to LTV

--- src/LTV.sol
+++ src/LTV.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.13;
 
 import "./borrowVault/PreviewDeposit.sol";
 import "./borrowVault/PreviewWithdraw.sol";
+import "./borrowVault/PreviewMint.sol";
+import "./borrowVault/PreviewRedeem.sol";
 
-contract LTV is PreviewWithdraw, PreviewDeposit {
+contract LTV is PreviewWithdraw, PreviewDeposit, PreviewMint, PreviewRedeem {
 
     constructor(string memory _name, uint _value) ERC20("LTV", "LTV", 18) {
         //
