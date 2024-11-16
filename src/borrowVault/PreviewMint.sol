0795268ab75c8b377b9e06eeffb2eface423b46f
Andrey Sobol <andreysobol@protonmail.com>
2024-11-16 20:39:52 +0700

    pmb

---
+++ src/borrowVault/PreviewMint.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+import "../State.sol";
+import "../Constants.sol";
+import "../Structs.sol";
+import "./totalAssets.sol";
+import "../ERC20.sol";
+import "../Cases.sol";
+import "../math/MintRedeamBorrow.sol";
+
+abstract contract PreviewMint is State, MintRedeamBorrow {
+
+    using MulDiv for uint256;
+
+    function previewMint(uint256 shares) external view returns (uint256 assets) {
+
+        int256 signedAssets = previewMintRedeamBorrow(-1*int256(assets));
+        // int256 signedShares = previewMintRedeamBorrow(-1*int256(assets));
+
+        if (signedAssets > 0) {
+            return 0;
+        }
+
+        return uint256(signedAssets);
+    }
+
+}
