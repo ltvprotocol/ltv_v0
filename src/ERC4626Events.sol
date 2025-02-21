838472a03e8bb526a3c391f9430720961a5d043e
Andrey Sobol <andreysobol@protonmail.com>
2025-02-21 11:39:41 -0700

    Solidity version 0.8.28

--- src/ERC4626Events.sol
+++ src/ERC4626Events.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.28;
 
 contract ERC4626Events {
   event Deposit(address sender, address owner, uint256 assets, uint256 shares);
--- src/dummy/interfaces/IDummyOracle.sol
+++ src/dummy/interfaces/IDummyOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.28;
 
 interface IDummyOracle {
   function getAssetPrice(address asset) external view returns (uint256);
