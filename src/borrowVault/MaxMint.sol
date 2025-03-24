601a21c69ee50ea2ac65ed766bd05ed6ce413b2f
Vladyslav Burtsevych <vladm2323@gmail.com>
2025-03-24 15:04:06 +0100

    add: max mint rounding

--- src/borrowVault/MaxMint.sol
+++ src/borrowVault/MaxMint.sol
@@ -11,12 +11,14 @@ abstract contract MaxMint is PreviewDeposit {
 
         uint256 availableSpaceInShares = getAvailableSpaceInShares(convertedAssets, previewSupplyAfterFee());
 
-        uint256 minProfitRealBorrow = uint256(convertedAssets.realCollateral).mulDivDown(minProfitLTV, Constants.LTV_DIVIDER);
+        // round up to assume smaller border
+        uint256 minProfitRealBorrow = uint256(convertedAssets.realCollateral).mulDivUp(minProfitLTV, Constants.LTV_DIVIDER);
         if (uint256(convertedAssets.realBorrow) <= minProfitRealBorrow) {
             return 0;
         }
 
         uint256 maxDepositInUnderlying = uint256(convertedAssets.realBorrow) - minProfitRealBorrow;
+        // round down to assume smaller border
         uint256 maxDepositInAssets = maxDepositInUnderlying.mulDivDown(Constants.ORACLE_DIVIDER, getPriceBorrowOracle());
         uint256 maxMintShares = previewDeposit(maxDepositInAssets);
 
