88d14b86f4b98dbd085da431aa42d717f1a14451
Vladyslav Burtsevych <vladm2323@gmail.com>
2025-03-24 15:09:08 +0100

    add: max withdraw rounding

--- src/borrowVault/MaxWithdraw.sol
+++ src/borrowVault/MaxWithdraw.sol
@@ -4,18 +4,21 @@ pragma solidity ^0.8.28;
 import '../MaxGrowthFee.sol';
 
 abstract contract MaxWithdraw is MaxGrowthFee {
-  using uMulDiv for uint256;
+    using uMulDiv for uint256;
 
-  function maxWithdraw(address owner) public view returns(uint256) {
-    ConvertedAssets memory convertedAssets = recoverConvertedAssets();
-    uint256 maxSafeRealBorrow = uint256(convertedAssets.realCollateral).mulDivDown(maxSafeLTV, Constants.LTV_DIVIDER);
-    if (maxSafeRealBorrow <= uint256(convertedAssets.realBorrow)) {
-      return 0;
+    function maxWithdraw(address owner) public view returns (uint256) {
+        ConvertedAssets memory convertedAssets = recoverConvertedAssets();
+        // round down to assume smaller border
+        uint256 maxSafeRealBorrow = uint256(convertedAssets.realCollateral).mulDivDown(maxSafeLTV, Constants.LTV_DIVIDER);
+        if (maxSafeRealBorrow <= uint256(convertedAssets.realBorrow)) {
+            return 0;
+        }
+        uint256 maxWithdrawInUnderlying = maxSafeRealBorrow - uint256(convertedAssets.realBorrow);
+        // round down to assume smaller border
+        uint256 vaultMaxWithdraw = maxWithdrawInUnderlying.mulDivDown(Constants.ORACLE_DIVIDER, getPriceBorrowOracle());
+        // round down to assume smaller border
+        uint256 userBalanceInAssets = balanceOf[owner].mulDivDown(totalAssets(), previewSupplyAfterFee());
+
+        return userBalanceInAssets < vaultMaxWithdraw ? userBalanceInAssets : vaultMaxWithdraw;
     }
-    uint256 maxWithdrawInUnderlying = maxSafeRealBorrow - uint256(convertedAssets.realBorrow);
-    uint256 vaultMaxWithdraw = maxWithdrawInUnderlying.mulDivDown(Constants.ORACLE_DIVIDER, getPriceBorrowOracle());
-    uint256 userBalanceInAssets = balanceOf[owner].mulDivDown(totalAssets(), previewSupplyAfterFee());
-
-    return userBalanceInAssets < vaultMaxWithdraw ? userBalanceInAssets : vaultMaxWithdraw;
-  }
 }
