34d4b6493c0c7d1ebcef2a48913c4f9ed5cb69e6
vlad.burtsevych <vladm2323@gmail.com>
2025-03-24 16:27:18 +0100

    add: rounding for previewMint

--- src/borrowVault/PreviewMint.sol
+++ src/borrowVault/PreviewMint.sol
@@ -1,24 +1,25 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.28;
 
-import "../Constants.sol";
-import "../math/MintRedeem.sol";
+import '../Constants.sol';
+import '../math/MintRedeem.sol';
 import '../MaxGrowthFee.sol';
 
 abstract contract PreviewMint is MaxGrowthFee {
     using uMulDiv for uint256;
 
     function previewMint(uint256 shares) public view returns (uint256 assets) {
-        uint256 sharesInAssets = shares.mulDivUp(totalAssets(), previewSupplyAfterFee());
-        uint256 sharesInUnderlying = sharesInAssets.mulDivUp(getPrices().borrow, Constants.ORACLE_DIVIDER);
-
+        // round up to receive more assets
         Prices memory prices = getPrices();
+        uint256 sharesInUnderlying = shares.mulDivUp(totalAssets(), previewSupplyAfterFee()).mulDivUp(prices.borrow, Constants.ORACLE_DIVIDER);
+
         int256 assetsInUnderlying = MintRedeem.previewMintRedeem(int256(sharesInUnderlying), true, recoverConvertedAssets(), prices, targetLTV);
 
         if (assetsInUnderlying > 0) {
             return 0;
         }
 
-        return uint256(-assetsInUnderlying).mulDivDown(Constants.ORACLE_DIVIDER, prices.borrow);
+        // round up to receive more assets
+        return uint256(-assetsInUnderlying).mulDivUp(Constants.ORACLE_DIVIDER, prices.borrow);
     }
 }
