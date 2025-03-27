498d675c771c647a5267d99c828332c87c827843
Vladyslav Burtsevych <vladm2323@gmail.com>
2025-03-27 13:04:01 +0100

    fix: compile fix

--- src/State.sol
+++ src/State.sol
@@ -9,13 +9,13 @@ import './utils/MulDiv.sol';
 
 import 'forge-std/interfaces/IERC20.sol';
 
-import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import '@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol';
 
 import './interfaces/ILendingConnector.sol';
 import './interfaces/IOracleConnector.sol';
 
-abstract contract State is OwnableUpgradeable, ReentrancyGuardUpgradeable {
+abstract contract State is Initializable, ReentrancyGuardUpgradeable {
     using uMulDiv for uint256;
     using sMulDiv for int256;
 
