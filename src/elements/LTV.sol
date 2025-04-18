50ccfd4c6bc175d31bfbf6582327fa98fc06e608
Vladyslav Burtsevych <vladm2323@gmail.com>
2025-04-18 17:36:52 +0200

    fix: renaming

--- src/elements/LTV.sol
+++ src/elements/LTV.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.28;
 
-import "../facades/reads/AutcionRead.sol";
+import "../facades/reads/AuctionRead.sol";
 import "../facades/writes/AuctionWrite.sol";
 import '../facades/reads/ERC20Read.sol';
 import '../facades/writes/ERC20Write.sol';
