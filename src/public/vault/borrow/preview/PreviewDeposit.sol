cd56a252c393ce6bf173a3913c723ac3fceb78a5
Vladyslav Burtsevych <vladm2323@gmail.com>
2025-04-19 19:35:15 +0200

    add: preview deposit

---
+++ src/public/vault/borrow/preview/PreviewDeposit.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.28;
+
+import '../Vault.sol';
+import '../../../../math2/DepositWithdraw.sol';
+
+abstract contract PreviewDeposit is Vault {
+    using uMulDiv for uint256;
+
+    function previewDeposit(uint256 assets, VaultState memory state) public pure returns (uint256 shares) {
+        return _previewDeposit(assets, vaultStateToData(state));
+    }
+
+    function _previewDeposit(uint256 assets, VaultData memory data) internal pure returns (uint256 shares) {
+        // depositor/withdrawer <=> HODLer conflict, assume user deposits less to mint less shares
+        uint256 assetsInUnderlying = assets.mulDivDown(data.borrowPrice, Constants.ORACLE_DIVIDER);
+    
+        int256 sharesInUnderlying = DepositWithdraw.previewDepositWithdraw(
+            DepositWithdrawData({
+                collateral: data.collateral,
+                borrow: data.borrow,
+                futureBorrow: data.futureBorrow,
+                futureCollateral: data.futureCollateral,
+                userFutureRewardBorrow: data.userFutureRewardBorrow,
+                userFutureRewardCollateral: data.userFutureRewardCollateral,
+                protocolFutureRewardBorrow: data.protocolFutureRewardBorrow,
+                protocolFutureRewardCollateral: data.protocolFutureRewardCollateral,
+                collateralSlippage: data.collateralSlippage,
+                borrowSlippage: data.borrowSlippage,
+                targetLTV: data.targetLTV,
+                deltaRealCollateral: 0,
+                deltaRealBorrow: -1 * int256(assetsInUnderlying)
+            })
+        );
+
+        if (sharesInUnderlying < 0) {
+            return 0;
+        }
+
+        // HODLer <=> depositor conflict, resolve in favor of HODLer, round down to mint less shares
+        return uint256(sharesInUnderlying).mulDivDown(Constants.ORACLE_DIVIDER, data.borrowPrice).mulDivDown(data.supplyAfterFee, data.totalAssets);
+    }
+} 
