36ac94f1bde4f9901a07e9aaf719419866e71c88
Vladyslav Burtsevych <vladm2323@gmail.com>
2025-05-01 14:39:44 +0200

    refactor: better module-impl structure

--- src/elements/ERC20Module.sol
+++ src/elements/ERC20Module.sol
@@ -1,34 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.28;
 
-import 'src/state_transition/Whitelist.sol';
-import 'src/state_transition/FunctionStopper.sol';
-import 'src/events/ERC20Events.sol';
-import '@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol';
+import 'src/public/erc20/ERC20WriteImpl.sol';
 
-contract ERC20Module is Whitelist, FunctionStopper, ReentrancyGuardUpgradeable, ERC20Events {
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external isFunctionAllowed isReceiverWhitelisted(recipient) nonReentrant returns (bool) {
-        allowance[sender][msg.sender] -= amount;
-        balanceOf[sender] -= amount;
-        balanceOf[recipient] += amount;
-        emit Transfer(sender, recipient, amount);
-        return true;
-    }
-
-    function transfer(address recipient, uint256 amount) external isFunctionAllowed isReceiverWhitelisted(recipient) nonReentrant returns (bool) {
-        balanceOf[msg.sender] -= amount;
-        balanceOf[recipient] += amount;
-        emit Transfer(msg.sender, recipient, amount);
-        return true;
-    }
-
-    function approve(address spender, uint256 amount) external isFunctionAllowed nonReentrant returns (bool) {
-        allowance[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
-        return true;
-    }
-}
+contract ERC20Module is ERC20WriteImpl {}
