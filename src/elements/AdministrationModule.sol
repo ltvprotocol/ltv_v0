634dd7277cce232d5b1d60bf01956b7e434f4933
Vladyslav Burtsevych <vladm2323@gmail.com>
2025-05-15 15:59:23 +0200

    fix: merge fix

--- src/elements/AdministrationModule.sol
+++ src/elements/AdministrationModule.sol
@@ -119,7 +119,8 @@ contract AdministrationModule is
         futureRewardCollateralAssets = 0;
         startAuction = 0;
 
-        uint256 realBorrowAssets = lendingConnector.getRealBorrowAssets();
+        // round up to repay all borrow
+        uint256 realBorrowAssets = lendingConnector.getRealBorrowAssets(false);
 
         require(closeAmountBorrow >= realBorrowAssets, ImpossibleToCoverDeleverage(realBorrowAssets, closeAmountBorrow));
 
@@ -132,7 +133,9 @@ contract AdministrationModule is
             borrowToken.transferFrom(msg.sender, address(this), realBorrowAssets);
             repay(realBorrowAssets);
         }
-        withdraw(lendingConnector.getRealCollateralAssets());
+
+        // round down to make successful withdraw
+        withdraw(lendingConnector.getRealCollateralAssets(false));
 
         if (collateralToTransfer != 0) {
             collateralToken.transfer(msg.sender, collateralToTransfer);
