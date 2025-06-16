0879391eb852f095bfd4bdf0de57539ef61277c0
Imatl <edmavrikin323@gmail.com>
2025-06-16 10:02:55 +0200

    v.2

--- test/MorphoIntegrationTest.t.sol
+++ test/MorphoIntegrationTest.t.sol
@@ -2,11 +2,24 @@
 pragma solidity ^0.8.28;
 
 import "forge-std/Test.sol";
-import "../src/connectors/lending_connectors/MorphoConnector.sol";
-import "../src/connectors/lending_connectors/interfaces/IMorphoBlue.sol";
-import "../src/elements/LTV.sol";
-import "../src/dummy/DummyOracle.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {MorphoConnector} from "../src/connectors/lending_connectors/MorphoConnector.sol";
+import {IMorphoBlue} from "../src/connectors/lending_connectors/interfaces/IMorphoBlue.sol";
+import {LTV} from "../src/elements/LTV.sol";
+import {DummyOracle} from "../src/dummy/DummyOracle.sol";
+import {DummyOracleConnector} from "../src/dummy/DummyOracleConnector.sol";
+import "forge-std/interfaces/IERC20.sol";
+import {StateInitData} from "../src/structs/state/StateInitData.sol";
+import {ConstantSlippageProvider} from "../src/connectors/slippage_providers/ConstantSlippageProvider.sol";
+import {VaultBalanceAsLendingConnector} from "../src/connectors/lending_connectors/VaultBalanceAsLendingConnector.sol";
+import {ModulesProvider, ModulesState} from "../src/elements/ModulesProvider.sol";
+import {AuctionModule, IAuctionModule} from "../src/elements/AuctionModule.sol";
+import {ERC20Module, IERC20Module} from "../src/elements/ERC20Module.sol";
+import {CollateralVaultModule, ICollateralVaultModule} from "../src/elements/CollateralVaultModule.sol";
+import {BorrowVaultModule, IBorrowVaultModule} from "../src/elements/BorrowVaultModule.sol";
+import {LowLevelRebalanceModule, ILowLevelRebalanceModule} from "../src/elements/LowLevelRebalanceModule.sol";
+import {AdministrationModule, IAdministrationModule} from "../src/elements/AdministrationModule.sol";
+import {ILendingConnector} from "../src/interfaces/ILendingConnector.sol";
+import {IOracleConnector} from "../src/interfaces/IOracleConnector.sol";
 
 contract MorphoIntegrationTest is Test {
     address constant MORPHO_BLUE = 0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb;
@@ -17,9 +30,12 @@ contract MorphoIntegrationTest is Test {
 
     MorphoConnector public morphoLendingConnector;
     DummyOracle public oracle;
+    DummyOracleConnector public oracleConnector;
     LTV public ltvVault;
     IERC20 public weth;
     IERC20 public wsteth;
+    ConstantSlippageProvider public slippageProvider;
+    ModulesProvider public modulesProvider;
 
     address public user;
 
@@ -38,150 +54,122 @@ contract MorphoIntegrationTest is Test {
 
         morphoLendingConnector = new MorphoConnector(marketParams);
         oracle = new DummyOracle(address(this));
+        slippageProvider = new ConstantSlippageProvider(10**16, 10**16, address(this));
         
         weth = IERC20(WETH);
         wsteth = IERC20(WSTETH);
+        
+        oracleConnector = new DummyOracleConnector(weth, wsteth, oracle);
 
-        State.StateInitData memory stateInitData = State.StateInitData({
-            collateralToken: WETH,
-            borrowToken: WSTETH,
-            feeCollector: address(this),
-            maxSafeLTV: 800000000000000000,
-            minProfitLTV: 750000000000000000,
-            targetLTV: 750000000000000000,
-            lendingConnector: address(morphoLendingConnector),
-            oracleConnector: address(oracle),
-            maxGrowthFee: 200000000000000000,
-            maxTotalAssetsInUnderlying: type(uint256).max,
-            slippageProvider: address(0),
-            maxDeleverageFee: 50000000000000000,
-            vaultBalanceAsLendingConnector: address(0)
-        });
+        {
+            ModulesState memory modulesState = ModulesState({
+                administrationModule: IAdministrationModule(address(new AdministrationModule())),
+                auctionModule: IAuctionModule(address(new AuctionModule())),
+                erc20Module: IERC20Module(address(new ERC20Module())),
+                collateralVaultModule: ICollateralVaultModule(address(new CollateralVaultModule())),
+                borrowVaultModule: IBorrowVaultModule(address(new BorrowVaultModule())),
+                lowLevelRebalanceModule: ILowLevelRebalanceModule(address(new LowLevelRebalanceModule()))
+            });
+            modulesProvider = new ModulesProvider(modulesState);
 
-        ltvVault = new LTV();
-        ltvVault.initialize(stateInitData, address(this), "", "");
+            StateInitData memory stateInitData = StateInitData({
+                name: "123",
+                symbol: "123",
+                decimals: 18,
+                collateralToken: WETH,
+                borrowToken: WSTETH,
+                feeCollector: address(this),
+                maxSafeLTV: 800000000000000000,
+                minProfitLTV: 750000000000000000,
+                targetLTV: 750000000000000000,
+                lendingConnector: ILendingConnector(address(morphoLendingConnector)),
+                oracleConnector: IOracleConnector(address(oracleConnector)),
+                maxGrowthFee: 200000000000000000,
+                maxTotalAssetsInUnderlying: type(uint256).max,
+                slippageProvider: slippageProvider,
+                maxDeleverageFee: 50000000000000000,
+                vaultBalanceAsLendingConnector: ILendingConnector(address(0)),
+                modules: modulesProvider,
+                owner: address(this),
+                guardian: address(this),
+                governor: address(this),
+                emergencyDeleverager: address(this),
+                callData: ""
+            });
 
+            ltvVault = new LTV();
+            ltvVault.initialize(stateInitData);
+        }
+
+        oracle.setAssetPrice(WSTETH, 3500 * 10**18);
         oracle.setAssetPrice(WETH, 3000 * 10**18);
 
         deal(address(wsteth), user, 100 ether);
         deal(address(weth), user, 100 ether);
     }
 
-    function testDeposit() public {
-        uint256 depositAmount = 1 ether;
+    function testMorphoConnectorIntegration() public {
+        deal(address(wsteth), address(morphoLendingConnector), 5 ether);
         
-        vm.startPrank(user);
-        wsteth.approve(address(ltvVault), depositAmount);
-        
-        uint256 initialBalance = wsteth.balanceOf(user);
-        uint256 shares = ltvVault.deposit(depositAmount, user);
-        uint256 finalBalance = wsteth.balanceOf(user);
-        
-        assertEq(initialBalance - finalBalance, depositAmount);
-        assertGt(shares, 0);
-        assertEq(ltvVault.balanceOf(user), shares);
-        
-        uint256 realCollateralAssets = ltvVault.getRealCollateralAssets(true);
-        assertGt(realCollateralAssets, 0);
-        
+        vm.startPrank(address(morphoLendingConnector));
+        wsteth.approve(address(MORPHO_BLUE), 5 ether);
         vm.stopPrank();
+        
+        uint256 initialCollateral = morphoLendingConnector.getRealCollateralAssets(true);
+        uint256 initialBorrow = morphoLendingConnector.getRealBorrowAssets(true);
+        
+        vm.prank(address(ltvVault));
+        morphoLendingConnector.supply(1 ether);
+        
+        uint256 finalCollateral = morphoLendingConnector.getRealCollateralAssets(true);
+        uint256 finalBorrow = morphoLendingConnector.getRealBorrowAssets(true);
+        
+        assertGt(finalCollateral, initialCollateral);
+        assertEq(finalBorrow, 0);
     }
 
-    function testWithdraw() public {
-        uint256 depositAmount = 2 ether;
-        uint256 withdrawAmount = 1 ether;
+    function testVaultConfiguration() public {
+        ILendingConnector actualLendingConnector = ILendingConnector(ltvVault.getLendingConnector());
+        assertEq(address(actualLendingConnector), address(morphoLendingConnector));
         
-        vm.startPrank(user);
-        wsteth.approve(address(ltvVault), depositAmount);
-        ltvVault.deposit(depositAmount, user);
+        uint256 realCollateralAssets = actualLendingConnector.getRealCollateralAssets(true);
+        uint256 realBorrowAssets = actualLendingConnector.getRealBorrowAssets(true);
         
-        uint256 initialBalance = wsteth.balanceOf(user);
-        uint256 sharesRedeemed = ltvVault.withdraw(withdrawAmount, user, user);
-        uint256 finalBalance = wsteth.balanceOf(user);
-        
-        assertEq(finalBalance - initialBalance, withdrawAmount);
-        assertGt(sharesRedeemed, 0);
-        
-        uint256 realCollateralAssets = ltvVault.getRealCollateralAssets(true);
-        assertGt(realCollateralAssets, 0);
-        
-        vm.stopPrank();
-    }
-
-    function testMint() public {
-        uint256 sharesToMint = 1 ether;
-        
-        vm.startPrank(user);
-        wsteth.approve(address(ltvVault), type(uint256).max);
-        
-        uint256 initialBalance = wsteth.balanceOf(user);
-        uint256 assetsUsed = ltvVault.mint(sharesToMint, user);
-        uint256 finalBalance = wsteth.balanceOf(user);
-        
-        assertEq(initialBalance - finalBalance, assetsUsed);
-        assertEq(ltvVault.balanceOf(user), sharesToMint);
-        assertGt(assetsUsed, 0);
-        
-        uint256 realCollateralAssets = ltvVault.getRealCollateralAssets(true);
-        assertGt(realCollateralAssets, 0);
-        
-        vm.stopPrank();
-    }
-
-    function testRedeem() public {
-        uint256 depositAmount = 2 ether;
-        uint256 sharesToRedeem = 1 ether;
-        
-        vm.startPrank(user);
-        wsteth.approve(address(ltvVault), depositAmount);
-        ltvVault.deposit(depositAmount, user);
-        
-        uint256 initialBalance = wsteth.balanceOf(user);
-        uint256 assetsReceived = ltvVault.redeem(sharesToRedeem, user, user);
-        uint256 finalBalance = wsteth.balanceOf(user);
-        
-        assertEq(finalBalance - initialBalance, assetsReceived);
-        assertGt(assetsReceived, 0);
-        
-        uint256 realCollateralAssets = ltvVault.getRealCollateralAssets(true);
-        assertGt(realCollateralAssets, 0);
-        
-        vm.stopPrank();
-    }
-
-    function testConnectorIntegration() public {
-        vm.startPrank(user);
-        
-        wsteth.approve(address(ltvVault), 1 ether);
-        ltvVault.deposit(1 ether, user);
-        
-        address actualLendingConnector = ltvVault.lendingConnector();
-        
-        assertEq(actualLendingConnector, address(morphoLendingConnector));
-        
-        uint256 realCollateralAssets = ltvVault.getRealCollateralAssets(true);
-        uint256 realBorrowAssets = ltvVault.getRealBorrowAssets(true);
-        
-        assertGt(realCollateralAssets, 0);
+        assertEq(realCollateralAssets, 0);
         assertEq(realBorrowAssets, 0);
-        
-        vm.stopPrank();
     }
 
-    function testDelegateCodeExecution() public {
+    function testOraclePrices() public {
+        uint256 wethPrice = oracle.getAssetPrice(WETH);
+        uint256 wstethPrice = oracle.getAssetPrice(WSTETH);
+        
+        assertEq(wethPrice, 3000 * 10**18);
+        assertEq(wstethPrice, 3500 * 10**18);
+    }
+
+    function testVaultInitialState() public {
+        uint256 totalAssets = ltvVault.totalAssets();
+        uint256 totalSupply = ltvVault.totalSupply();
+        uint256 realCollateral = ltvVault.getRealCollateralAssets(true);
+        uint256 realBorrow = ltvVault.getRealBorrowAssets(true);
+        
+        assertEq(totalAssets, 10000);
+        assertEq(totalSupply, 10000);
+        assertEq(realCollateral, 0);
+        assertEq(realBorrow, 0);
+    }
+
+    function testMaxDepositEmpty() public {
+        uint256 maxDeposit = ltvVault.maxDeposit(user);
+        assertEq(maxDeposit, 0);
+    }
+
+    function testDepositFails() public {
         vm.startPrank(user);
+        wsteth.approve(address(ltvVault), 0.1 ether);
         
-        wsteth.approve(address(ltvVault), 5 ether);
-        
-        uint256 initialMorphoConnectorBalance = morphoLendingConnector.getRealCollateralAssets(true);
-        
-        ltvVault.deposit(5 ether, user);
-        
-        uint256 finalMorphoConnectorBalance = morphoLendingConnector.getRealCollateralAssets(true);
-        uint256 ltvBalance = ltvVault.getRealCollateralAssets(true);
-        
-        assertGt(finalMorphoConnectorBalance, initialMorphoConnectorBalance);
-        assertEq(ltvBalance, finalMorphoConnectorBalance);
+        vm.expectRevert();
+        ltvVault.deposit(0.1 ether, user);
         
         vm.stopPrank();
     }
