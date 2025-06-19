4cb759b253c7f7ede8315ec11cd1e960276dc320
Vladyslav Burtsevych <vladm2323@gmail.com>
2025-06-19 12:31:44 +0200

    add: auction availability test

---
+++ test/auction/AuctionTestCommon.t.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.28;
+
+import {BaseTest, DefaultTestData, Constants} from "../utils/BaseTest.t.sol";
+
+contract AuctionTestCommon is BaseTest {
+    struct AuctionState {
+        int256 futureBorrowAssets;
+        int256 futureCollateralAssets;
+        int256 futureRewardBorrowAssets;
+        int256 futureRewardCollateralAssets;
+    }
+
+    function getAuctionState() internal view returns (AuctionState memory) {
+        return AuctionState({
+            futureBorrowAssets: ltv.futureBorrowAssets(),
+            futureCollateralAssets: ltv.futureCollateralAssets(),
+            futureRewardBorrowAssets: ltv.futureRewardBorrowAssets(),
+            futureRewardCollateralAssets: ltv.futureRewardCollateralAssets()
+        });
+    }
+
+    function abs(int256 a) internal pure returns (int256) {
+        return a < 0 ? -a : a;
+    }
+
+    function checkFutureExecutorProfit(AuctionState memory initialAuctionState) internal view {
+        AuctionState memory auctionState = getAuctionState();
+
+        int256 oldReward = (initialAuctionState.futureBorrowAssets + initialAuctionState.futureRewardBorrowAssets)
+            - (initialAuctionState.futureCollateralAssets + initialAuctionState.futureRewardCollateralAssets) * 2;
+
+
+        int256 newReward = (auctionState.futureBorrowAssets + auctionState.futureRewardBorrowAssets)
+            - (auctionState.futureCollateralAssets + auctionState.futureRewardCollateralAssets) * 2;
+
+        assertGe(newReward, 0, "newReward is not positive");
+
+        assertGe(
+            abs(initialAuctionState.futureBorrowAssets) * newReward, abs(auctionState.futureBorrowAssets) * oldReward
+        );
+    }
+
+    function prepareUser(address user) public {
+        deal(address(collateralToken), user, type(uint256).max);
+        deal(address(borrowToken), user, type(uint256).max);
+
+        vm.startPrank(user);
+        collateralToken.approve(address(ltv), type(uint256).max);
+        borrowToken.approve(address(ltv), type(uint256).max);
+        vm.stopPrank();
+    }
+
+    function prepareWithdrawAuction(uint128 amount, address governor, address user) public {
+        vm.startPrank(governor);
+        ltv.setMinProfitLTV(uint128(0));
+        ltv.setMaxSafeLTV(uint128(Constants.LTV_DIVIDER));
+        vm.stopPrank();
+
+        vm.startPrank(user);
+        deal(address(collateralToken), user, type(uint256).max);
+        collateralToken.approve(address(ltv), type(uint256).max);
+        ltv.depositCollateral(amount, user);
+
+        ltv.executeLowLevelRebalanceShares(0);
+
+        ltv.setFutureBorrowAssets(-int256(uint256(amount)));
+        ltv.setFutureCollateralAssets(-int256(uint256(amount)) / 2 + (amount % 2 == 1 ? -1 : int8(0)));
+        ltv.setFutureRewardBorrowAssets(int256(uint256(amount / 100)));
+        deal(address(collateralToken), user, 0);
+    }
+
+    function prepareDepositAuction(uint128 amount) public {
+        ltv.setFutureBorrowAssets(int256(uint256(amount)));
+        ltv.setFutureCollateralAssets(int256(uint256(amount)) / 2 );
+        ltv.setFutureRewardCollateralAssets(-int256(uint256(amount / 100)));
+    }
+}
---
+++ test/auction/ExecutionAvailablility.t.sol
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.28;
+
+import "forge-std/Test.sol";
+import "./AuctionTestCommon.t.sol";
+import "../../src/errors/IAuctionErrors.sol";
+
+contract ExecutionAvailablilityTest is AuctionTestCommon {
+    function test_failIfOppositeSignWithdrawAuction(DefaultTestData memory data, address user, uint120 amount)
+        public
+        testWithPredefinedDefaultValues(data)
+    {
+        prepareWithdrawAuction(amount, data.governor, user);
+
+        int256 deltaFutureBorrowAssetsOppositeSign = ltv.futureBorrowAssets() / 2;
+        int256 deltaFutureCollateralAssetsOppositeSign = ltv.futureCollateralAssets() / 2;
+        vm.startPrank(user);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAuctionErrors.NoAuctionForProvidedDeltaFutureBorrow.selector,
+                ltv.futureBorrowAssets(),
+                ltv.futureRewardBorrowAssets(),
+                deltaFutureBorrowAssetsOppositeSign
+            )
+        );
+
+        ltv.executeAuctionBorrow(deltaFutureBorrowAssetsOppositeSign);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAuctionErrors.NoAuctionForProvidedDeltaFutureCollateral.selector,
+                ltv.futureCollateralAssets(),
+                ltv.futureRewardCollateralAssets(),
+                deltaFutureCollateralAssetsOppositeSign
+            )
+        );
+        ltv.executeAuctionCollateral(deltaFutureCollateralAssetsOppositeSign);
+    }
+
+    function test_failIfOppositeSignDepositAuction(DefaultTestData memory data, address user, uint120 amount)
+        public
+        testWithPredefinedDefaultValues(data)
+    {
+        prepareUser(user);
+        prepareDepositAuction(amount);
+
+        int256 deltaFutureBorrowAssetsOppositeSign = ltv.futureBorrowAssets() / 2;
+        int256 deltaFutureCollateralAssetsOppositeSign = ltv.futureCollateralAssets() / 2;
+        vm.startPrank(user);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAuctionErrors.NoAuctionForProvidedDeltaFutureBorrow.selector,
+                ltv.futureBorrowAssets(),
+                ltv.futureRewardBorrowAssets(),
+                deltaFutureBorrowAssetsOppositeSign
+            )
+        );
+        ltv.executeAuctionBorrow(deltaFutureBorrowAssetsOppositeSign);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAuctionErrors.NoAuctionForProvidedDeltaFutureCollateral.selector,
+                ltv.futureCollateralAssets(),
+                ltv.futureRewardCollateralAssets(),
+                deltaFutureCollateralAssetsOppositeSign
+            )
+        );
+        ltv.executeAuctionCollateral(deltaFutureCollateralAssetsOppositeSign);
+    }
+
+    function test_failIfExceedsDepositAuctionSize(DefaultTestData memory data, uint120 amount)
+        public
+        testWithPredefinedDefaultValues(data)
+    {
+        prepareDepositAuction(amount);
+
+        int256 deltaFutureCollateralAssetsOverflow = -ltv.futureCollateralAssets() - 1;
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAuctionErrors.NoAuctionForProvidedDeltaFutureCollateral.selector,
+                ltv.futureCollateralAssets(),
+                ltv.futureRewardCollateralAssets(),
+                deltaFutureCollateralAssetsOverflow
+            )
+        );
+        ltv.executeAuctionCollateral(deltaFutureCollateralAssetsOverflow);
+
+        int256 deltaFutureBorrowAssetsOverflow = -ltv.futureBorrowAssets() - 1;
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAuctionErrors.NoAuctionForProvidedDeltaFutureBorrow.selector,
+                ltv.futureBorrowAssets(),
+                ltv.futureRewardBorrowAssets(),
+                deltaFutureBorrowAssetsOverflow
+            )
+        );
+        ltv.executeAuctionBorrow(deltaFutureBorrowAssetsOverflow);
+    }
+
+    function test_failIfExceedsWithdrawAuctionSize(DefaultTestData memory data, uint120 amount)
+        public
+        testWithPredefinedDefaultValues(data)
+    {
+        prepareWithdrawAuction(amount, data.governor, address(this));
+
+        int256 deltaFutureCollateralAssetsOverflow = -ltv.futureCollateralAssets() + 1;
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAuctionErrors.NoAuctionForProvidedDeltaFutureCollateral.selector,
+                ltv.futureCollateralAssets(),
+                ltv.futureRewardCollateralAssets(),
+                deltaFutureCollateralAssetsOverflow
+            )
+        );
+        ltv.executeAuctionCollateral(deltaFutureCollateralAssetsOverflow);
+
+        int256 deltaFutureBorrowAssetsOverflow = -ltv.futureBorrowAssets() + 1;
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAuctionErrors.NoAuctionForProvidedDeltaFutureBorrow.selector,
+                ltv.futureBorrowAssets(),
+                ltv.futureRewardBorrowAssets(),
+                deltaFutureBorrowAssetsOverflow
+            )
+        );
+        ltv.executeAuctionBorrow(deltaFutureBorrowAssetsOverflow);
+    }
+}
