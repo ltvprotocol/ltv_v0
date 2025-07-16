cf9e024c8b870e8bf5212c47b0d14b7185f1931a
Andrey Sobol <andreysobol@protonmail.com>
2025-07-16 19:37:51 +0100

    change comment

--- src/public/vault/collateral/max/MaxWithdrawCollateral.sol
+++ src/public/vault/collateral/max/MaxWithdrawCollateral.sol
@@ -13,7 +13,7 @@ abstract contract MaxWithdrawCollateral is PreviewWithdrawCollateral, PreviewRed
     }
 
     function _maxWithdrawCollateral(MaxWithdrawRedeemCollateralVaultData memory data) internal pure returns (uint256) {
-        // round down to assume smaller border
+        // round up to assume smaller border
         uint256 maxSafeRealCollateral = uint256(data.realBorrow).mulDivUp(Constants.LTV_DIVIDER, data.maxSafeLTV);
 
         if (maxSafeRealCollateral >= uint256(data.realCollateral)) {
