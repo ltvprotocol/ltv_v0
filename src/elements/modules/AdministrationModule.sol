ebd7f92dbfaddd3ebf21bd1c2bc523a5fc1589d2
Vladyslav Burtsevych <vladm2323@gmail.com>
2025-09-08 18:19:32 +0200

    refactor: administration public in administration folder

--- src/elements/modules/AdministrationModule.sol
+++ src/elements/modules/AdministrationModule.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.28;
 
-import {AdministrationPublic} from "src/public/AdministrationPublic.sol";
+import {AdministrationPublic} from "src/public/administration/AdministrationPublic.sol";
 
 contract AdministrationModule is AdministrationPublic {
     constructor() {
