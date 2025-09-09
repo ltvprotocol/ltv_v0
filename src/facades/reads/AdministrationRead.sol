375fa21dc14fd5135825a8b92bc0486971ef7de1
Vladyslav Burtsevych <vladm2323@gmail.com>
2025-09-09 19:43:00 +0200

    fix: move administration state reader into folder

--- src/facades/reads/AdministrationRead.sol
+++ src/facades/reads/AdministrationRead.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.28;
 
-import {GetLendingConnectorStateReader} from "../../state_reader/GetLendingConnectorStateReader.sol";
+import {GetLendingConnectorStateReader} from "../../state_reader/administration/GetLendingConnectorStateReader.sol";
 import {ILendingConnector} from "../../interfaces/connectors/ILendingConnector.sol";
 
 /**
--- src/state_transition/Lending.sol
+++ src/state_transition/Lending.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.28;
 import {ILendingConnector} from "../interfaces/connectors/ILendingConnector.sol";
 import {RevertWithDataIfNeeded} from "../utils/RevertWithDataIfNeeded.sol";
 import {GetLendingConnector} from "../public/administration/read/GetLendingConnector.sol";
-import {GetLendingConnectorStateReader} from "../state_reader/GetLendingConnectorStateReader.sol";
+import {GetLendingConnectorStateReader} from "../state_reader/administration/GetLendingConnectorStateReader.sol";
 
 abstract contract Lending is GetLendingConnectorStateReader, GetLendingConnector, RevertWithDataIfNeeded {
     function borrow(uint256 assets) internal {
