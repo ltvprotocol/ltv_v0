622cd8f65bf44cc6f636cb5cfc3a70ac29df0714
Vladyslav Burtsevych <vladm2323@gmail.com>
2025-09-16 13:25:32 +0200

    expected linter error

--- test/utils/DummyLTV.t.sol
+++ test/utils/DummyLTV.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.28;
 
 import {LTV} from "src/elements/LTV.sol";
+import {console} from "forge-std/console.sol";
 
 contract DummyLTV is LTV {
     function setLastSeenTokenPrice(uint256 value) public {
