528005fad56a7c9bc0d87543edd32959cb3a45ba
Vladyslav Burtsevych <vladm2323@gmail.com>
2025-10-20 18:38:00 +0200

    fix: proper delegate call post check

--- src/facades/writes/InitializeWrite.sol
+++ src/facades/writes/InitializeWrite.sol
@@ -5,7 +5,7 @@ import {IModules} from "src/interfaces/IModules.sol";
 import {IInitializeModule} from "src/interfaces/writes/IInitializeModule.sol";
 import {StateInitData} from "src/structs/state/initialize/StateInitData.sol";
 import {AdministrationWrite} from "src/facades/writes/AdministrationWrite.sol";
-import {RevertWithDataIfNeeded} from "src/utils/RevertWithDataIfNeeded.sol";
+import {DelegateCallPostCheck} from "src/utils/DelegateCallPostCheck.sol";
 
 /**
  * @title InitializeWrite
@@ -14,14 +14,15 @@ import {RevertWithDataIfNeeded} from "src/utils/RevertWithDataIfNeeded.sol";
  * the facade contract. After modules initialization, remaining part of LTV protocol can be initialized
  * via initialize module.
  */
-abstract contract InitializeWrite is AdministrationWrite, RevertWithDataIfNeeded {
+abstract contract InitializeWrite is AdministrationWrite, DelegateCallPostCheck {
     /**
      * @dev see ILTV.initialize
      */
     function initialize(StateInitData memory initData, IModules modules) external initializer {
         _setModules(modules);
+        address initializeModule = address(modules.initializeModule());
         (bool isSuccess, bytes memory data) =
-            address(modules.initializeModule()).delegatecall(abi.encodeCall(IInitializeModule.initialize, (initData)));
-        revertWithDataIfNeeded(isSuccess, data);
+            initializeModule.delegatecall(abi.encodeCall(IInitializeModule.initialize, (initData)));
+        delegateCallPostCheck(initializeModule, isSuccess, data);
     }
 }
---
+++ src/utils/DelegateCallPostCheck.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.28;
+
+import {IDelegateCallError} from "src/errors/IDelegateCallError.sol";
+/**
+ * @title DelegateCallPostCheck
+ * @notice contract contains functionality to rethrow revert error after delegatecall
+ */
+
+abstract contract DelegateCallPostCheck is IDelegateCallError {
+    /**
+     * @dev rethrows revert error after delegatecall
+     */
+    function delegateCallPostCheck(address target, bool isSuccess, bytes memory data) internal view {
+        if (!isSuccess) {
+            assembly {
+                revert(add(data, 32), mload(data))
+            }
+        }
+
+        require(data.length != 0 || target.code.length != 0, DelegateCallFailed());
+    }
+}
