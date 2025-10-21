eab5b53435a871e7ca1f244f4bf8a1f66198f55e
Vladyslav Burtsevych <vladm2323@gmail.com>
2025-10-21 19:10:54 +0200

    fix: typos

--- src/math/abstracts/MaxGrowthFee.sol
+++ src/math/abstracts/MaxGrowthFee.sol
@@ -16,7 +16,7 @@ abstract contract MaxGrowthFee is TotalAssets, TotalSupply {
      * @notice Calculates supply after applying max growth fee.
      *
      * @dev Calculations are derived from the ltv protocol paper.
-     * Makes sure fee collector receives fee by minting tokens untill
+     * Makes sure fee collector receives fee by minting tokens until
      * the deviation of current token price from last seen token
      * price will be equal to (100 - maxGrowthFee)%
      *
@@ -30,7 +30,7 @@ abstract contract MaxGrowthFee is TotalAssets, TotalSupply {
             return data.supply;
         }
 
-        // divident: asset * supply
+        // dividend: asset * supply
         // divisor: supply * maxGrowthFee * lastSeenTokenPrice + assets * (1 - maxGrowthFee)
 
         // underestimate new supply to mint less tokens
