0be94db2869003b7aa48e39e8e826988d189e48e
Vladyslav Burtsevych <vladm2323@gmail.com>
2025-10-22 16:07:32 +0200

    fix: spend allowance after possible early return

--- src/public/vault/write/collateral/execute/RedeemCollateral.sol
+++ src/public/vault/write/collateral/execute/RedeemCollateral.sol
@@ -55,15 +55,15 @@ abstract contract RedeemCollateral is
         uint256 max = _maxRedeemCollateral(data);
         require(shares <= max, ExceedsMaxRedeemCollateral(owner, shares, max));
 
-        if (owner != msg.sender) {
-            _spendAllowance(owner, msg.sender, shares);
-        }
-
         (uint256 assetsOut, DeltaFuture memory deltaFuture) =
             _previewRedeemCollateral(shares, data.previewCollateralVaultData);
 
         if (assetsOut == 0) {
             return 0;
+        }
+
+        if (owner != msg.sender) {
+            _spendAllowance(owner, msg.sender, shares);
         }
 
         applyMaxGrowthFee(
